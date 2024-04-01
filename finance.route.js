27ef1ce2e419376072ace97ec7c60f898a4e06ce
Darshna Limbani <139759647+darshnalimbani@users.noreply.github.com>
2024-04-01 10:57:44 +0530

    upload

---
+++ finance.route.js
@@ -0,0 +1,8 @@
+// routes/financeRoutes.js
+const express = require('express');
+const router = express.Router();
+const financeController = require('../controllers/financeController');
+
+router.post('/calculate', financeController.calculateFinanceScore);
+
+module.exports = router;
