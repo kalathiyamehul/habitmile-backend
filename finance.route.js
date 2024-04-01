e991bdbc3ad6dccb5d6c44fc6082adb467f1ca40
darshnalimbani <139759647+darshnalimbani@users.noreply.github.com>
2024-04-01 11:08:08 +0530

    foldre

--- finance.route.js
+++
@@ -1,8 +0,0 @@
-// routes/financeRoutes.js
-const express = require('express');
-const router = express.Router();
-const financeController = require('../controllers/financeController');
-
-router.post('/calculate', financeController.calculateFinanceScore);
-
-module.exports = router;
