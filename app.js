73d21727042dcc5386ff9cf71b2fbbcd493a4b9b
Darshna Limbani <139759647+darshnalimbani@users.noreply.github.com>
2024-04-03 15:15:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
+// // app.js
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const financeRoutes = require('./routes/finance.route');
+
+// const app = express();
+
+// app.use(express.json());
+
+// // Connect to MongoDB
+// mongoose.connect('mongodb+srv://tonylimbani:admin@cluster0.lrxmb3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch(err => console.error('Failed to connect to MongoDB', err));
+
+// app.use('/finance', financeRoutes);
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+
 // app.js
 const express = require('express');
 const mongoose = require('mongoose');
-const financeRoutes = require('./routes/finance.route');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://tonylimbani:admin@cluster0.lrxmb3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Failed to connect to MongoDB', err));
+// MongoDB connection
+mongoose.connect('mongodb+srv://tonylimbani:admin@cluster0.lrxmb3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
-app.use('/finance', financeRoutes);
+// Routes
+app.use('/api/user', userRoutes);
 
+// Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
