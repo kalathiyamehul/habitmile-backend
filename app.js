bb52ba53081cccbf51d78371b04eecaaec56c7c4
darshnalimbani <139759647+darshnalimbani@users.noreply.github.com>
2024-04-03 19:30:12 +0530

    cors added

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-// // app.js
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const financeRoutes = require('./routes/finance.route');
-
-// const app = express();
-
-// app.use(express.json());
-
-// // Connect to MongoDB
-// mongoose.connect('mongodb+srv://tonylimbani:admin@cluster0.lrxmb3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('Connected to MongoDB'))
-//     .catch(err => console.error('Failed to connect to MongoDB', err));
-
-// app.use('/finance', financeRoutes);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-
-// app.js
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
-
+const cors = require('cors');
 const app = express();
 app.use(express.json());
-
+app.use(cors({
+  origin: '*'
+}));
 // MongoDB connection
 mongoose.connect('mongodb+srv://hexablockstech:DkuBaPXYNrJ4Zse2@cluster0.ntmrtcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
@@ -44,7 +25,7 @@ app.use('/info', (req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
